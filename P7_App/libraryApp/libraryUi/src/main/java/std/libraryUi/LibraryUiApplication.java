c122fe05b84a3e370bb1a7d067a6f53874690b21
FredLeroux <f.leroux2903@free.fr>
2020-11-27 15:43:41 +0100

    intermediate

--- P7_App/libraryApp/libraryUi/src/main/java/std/libraryUi/LibraryUiApplication.java
+++ P7_App/libraryApp/libraryUi/src/main/java/std/libraryUi/LibraryUiApplication.java
@@ -1,10 +1,10 @@
 package std.libraryUi;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.SpringApplication;import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+
 
 
 
