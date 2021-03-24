adffa088108df2bf0424717dfca002a0eafb3cd5
FredLeroux <f.leroux2903@free.fr>
2021-03-24 19:56:17 +0100

    [Ticket 1] issue #5
    Correction micro-service libraryReservations

--- P7_App/libraryApp/libraryReservations/src/test/java/std/libraryReservations/LibraryReservationsApplicationTests.java
+++ P7_App/libraryApp/libraryReservations/src/test/java/std/libraryReservations/LibraryReservationsApplicationTests.java
@@ -1,13 +1,8 @@
 package std.libraryReservations;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class LibraryReservationsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
